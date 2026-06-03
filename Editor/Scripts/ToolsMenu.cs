7f98bd3474aaed7baa5c081fda70d20ad86ccb88
Philip Taylor <Oggal@users.noreply.github.com>
2026-06-02 22:49:03 -0500

    Add Editor scripts for ToolsMenu functionality

---
+++ Editor/Scripts/ToolsMenu.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+public static class ToolsMenu
+{
+    [MenuItem("Tools/Little Helpers/Open Persistent Data Folder")]
+    private static void OpenPersistentDataFolder()
+    {
+        string basePath = Application.persistentDataPath;
+
+        if (string.IsNullOrWhiteSpace(basePath))
+        {
+            Debug.LogWarning("Persistent data path is unavailable.");
+            return;
+        }
+
+        string projectFolder = Path.GetFileNameWithoutExtension(Application.dataPath);
+        string path = Path.Combine(basePath, projectFolder);
+        //Directory.CreateDirectory(path);
+
+
+        EditorUtility.RevealInFinder(path);
+    }
+}
